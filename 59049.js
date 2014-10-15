273adb9ede553eed8cc21c540df51a5888cf7189
Justine Jones <justinejones91@gmail.com>
2014-10-14 23:53:12 -0400

    arrow press controlls work

--- 59049.js
+++ 59049.js
@@ -1,11 +1,31 @@
 $(document).ready(function(){
-
+		var result = [];
 
 	function move() {
-		
+
+		$('body').keyup(function(event){
+			if (event.keyCode === 37) {
+				alert("you pressed the left button");
+			} else if (event.keyCode === 38) {
+				alert("you pressed the up button");
+			} else if (event.keyCode === 39) {
+				alert("you pressed the right button");
+			} else if (event.keyCode === 40){
+				alert("you pressed the down button");
+			}
+
+		});
+
+	};
+
+	move();
+
+	function newTile() {
+
 	};
 
 	function merge() {
+		var result = [];
 
 	};
 
