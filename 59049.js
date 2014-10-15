a2e3caf0e305213280a938e6b66b17c0f34eed61
Justine Jones <justinejones91@gmail.com>
2014-10-15 17:46:17 -0400

    fixed the slide function

--- 59049.js
+++ 59049.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
-		var result = [];
+		var turn = 0;
+		var grid = [[0,0,0,0],
+								[0,0,0,0],
+								[0,0,0,0],
+								[0,0,0,0]];
 
+
+
+	// the move function moves tiles in a particular direction based on which arrow is pressed
 	function move() {
 
-		$('body').keyup(function(event){
+		$(window).on('keyup', function(event){
 			if (event.keyCode === 37) {
 				alert("you pressed the left button");
 			} else if (event.keyCode === 38) {
@@ -15,19 +22,82 @@ $(document).ready(function(){
 			}
 
 		});
+	merge();
+	newTile();
+
+		turn++;
+		if (turn >= 10) {
+			win();
+		}
 
 	};
 
 	move();
+	// Adds a new tile to a random empty cell on the grid
+	function newTile(grid) {
+		var current = Math.floor((Math.random() * 4) + 2);
 
-	function newTile() {
+		if (current !== 2 || current !== 4) {
+
+		}
+
+		$('td').text().css({})
+		
+		Math.floor((Math.random() * 4));
 
 	};
 
-	function merge() {
+	// this function takes a row and slides all its values together
+
+	function slide(line){
 		var result = [];
 
+		for (var i = 0; i < line.length; i++) {
+			if (line[i] > 0) {
+				result.push(line[i]);	
+			}			
+		};
+
+		while (result.length < 4) {
+			result.push(0);
+		}
+		return result;
+		merge(result);
+	}
+l = [2,0,2,2];
+
+	// merge function adds adjacent tiles with the same number to form a new tile with their sum.
+	function merge(line) {
+		var merged_result = [];
+
+		for (var i = 0; i < line.length; i++) {
+			for (var j = i+1; j < line.length; j++) {
+				
+				if (line[i] === line[j]){
+					merged_result.push(line[i] + line[j]);
+
+				} else {
+					
+
+				}
+			};
+		};
 	};
+	
+
+
+
+
+
+	// win function checks for a winner after a certain amount of turns
+	function win() {
+
+	};
+
+
+
+
+	// play again function restarts game if confirmed yes
 
 	function playAgain() {
 		var answer = confirm("would you like to play again?")
