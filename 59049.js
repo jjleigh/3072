e60c1a2fa5fbf2eaf70d85f3886db4de7242df34
Justine Jones <justinejones91@gmail.com>
2014-10-14 21:15:54 -0400

    added the play again function

---
+++ 59049.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+
+
+
+	function playAgain() {
+		var answer = confirm("would you like to play again?")
+
+		if (answer) {
+			location.reload();
+		}
+
+	};
+
+
+
+});
